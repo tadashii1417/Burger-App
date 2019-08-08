79f07011b7d4296e1207a4aa754d63a3e43ca317
tadashii1417 <tadashii1417@gmail.com>
2019-08-08 22:14:26 +0700

    update

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Cover';
 import Style from './Layout.module.css';
 
 const layout = (props) => (
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Cover';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
